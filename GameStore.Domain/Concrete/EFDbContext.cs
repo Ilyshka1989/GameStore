cbfb9fa40d6055a352f30a25d2f91605774b3f0c
DESKTOP-HAERTJK\ilysh <ilyshka19890307@gmail.com>
2022-08-18 20:38:41 +0300

    add project

---
+++ GameStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,11 @@
+﻿using GameStore.Domain.Entities;
+using System.Data.Entity;
+//доступ к базе данных 
+
+namespace GameStore.Domain.Concrete
+{
+    public class EFDbContext: DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Configuration;
+using Ninject;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Concrete;
+using GameStore.WebUI.Infrastructure.Abstract;
+using GameStore.WebUI.Infrastructure.Concrete;
+// Папака распознаватель зависимостей
+namespace GameStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        // Привязки
+        private void AddBindings()
+        {
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                .AppSettings["Email.Write"] ?? "false")
+            };
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("setting", emailSettings);
+            kernel.Bind<IAuthProvider>().To<FormAuthProvider>();
+        }
+    }
+}
